242d69b01b2eade9fe96469b533d39b97d84a9f4
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-17 14:12:30 +0000

    Make sure the project has a name.

--- proj-sdl.zsh
+++ proj-sdl.zsh
@@ -1,5 +1,11 @@
 zsh $HOME/.zshide/lang-c.zsh
 
+project_name=$1
+if [[ -z $project_name ]]; then
+    print "E: no project name"
+    exit 1
+fi
+
 # TODO: Fedora specific for now.
 if [[ -z $(whence sdl2-config) ]]; then
     print "SDL2 not found. Trying to install it..."
