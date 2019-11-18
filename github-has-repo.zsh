67527d13aa39f20b650c6b1ee1ad9d748e49cdcb
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-18 18:47:24 +0000

    Cache repo list from GitHub.

--- github-has-repo.zsh
+++ github-has-repo.zsh
@@ -3,12 +3,16 @@ if [[ -z $ZI_PROJECT_NAME ]]; then
     exit 1
 fi
 
-token=$(cat $HOME/.zshide/tokens.txt | grep github | cut -d'=' -f2)
-auth="Authorization: token $token"
-accept="Accept: application/vnd.github.v3+json"
+repos=$HOME/.zshide/github-repos.json
+if [[ ! -f $repos ]]; then
+    token=$(cat $HOME/.zshide/tokens.txt | grep github | cut -d'=' -f2)
+    auth="Authorization: token $token"
+    accept="Accept: application/vnd.github.v3+json"
 
-url="https://api.github.com/user/repos"
-response=$(curl -H $auth -H $accept $url)
-repo_names=$(print $response | jq '.[].name' | tr -d '"')
+    url="https://api.github.com/user/repos"
+    curl -H $auth -H $accept $url > $repos
+fi
+
+repo_names=$(cat $repos | jq '.[].name' | tr -d '"')
 print $repo_names | grep ^$ZI_PROJECT_NAME$
 
