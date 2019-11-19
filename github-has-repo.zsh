9cac8aa7e74044835fa751db8cd90841c20919e5
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-19 09:33:15 +0000

    Call github-get-repos in github-has-repo.

--- github-has-repo.zsh
+++ github-has-repo.zsh
@@ -3,16 +3,8 @@ if [[ -z $ZI_PROJECT_NAME ]]; then
     exit 1
 fi
 
+zsh $ZI_HOME/github-get-repos.zsh
 repos=$HOME/.zshide/github-repos.json
-if [[ ! -f $repos ]]; then
-    token=$(cat $HOME/.zshide/tokens.txt | grep github | cut -d'=' -f2)
-    auth="Authorization: token $token"
-    accept="Accept: application/vnd.github.v3+json"
-
-    url="https://api.github.com/user/repos"
-    curl -H $auth -H $accept $url > $repos
-fi
-
 repo_names=$(cat $repos | jq '.[].name' | tr -d '"')
 print $repo_names | grep ^$ZI_PROJECT_NAME$
 
