cac8fdda49bf009b6083155fce6a04c9fcbd0009
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-19 09:08:10 +0000

    Added github-get-repos.

---
+++ github-get-repos.zsh
@@ -0,0 +1,26 @@
+pid_file=$ZI_HOME/github-get-repos.pid
+p=$(ps ax | grep $0 | grep --invert-match grep | awk '{ print $1}')
+
+if [[ -f $pid_file ]]; then
+    pid=$(cat $pid_file)
+
+    if [[ $pid != $p ]]; then
+        print "W: PIDs don't match" > /dev/stderr
+    else
+        exit 1
+    fi
+fi
+
+print $$ > $pid_file
+
+repos=$ZI_HOME/github-repos.json
+if [[ ! -f $repos ]]; then
+    token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
+    auth="Authorization: token $token"
+    accept="Accept: application/vnd.github.v3+json"
+
+    url="https://api.github.com/user/repos"
+    curl -H $auth -H $accept $url > $repos
+fi
+
+rm $pid_file
