d8c835142185dc5fb939036be6b303325ea4d30f
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-20 10:57:38 +0000

    Use the appropriate function for printing messages.

--- zi-create-new-project.zsh
+++ zi-create-new-project.zsh
@@ -7,19 +7,19 @@ elif [[ ! -f $ZI_HOME/proj-$2.zsh ]]; then
 fi
 
 if [[ -z $ZI_PROJECT_NAME ]]; then
-    print "E: no project name specified"
+    err "E: no project name specified"
     exit $E_PARAM
 fi
 
 if [[ -n $(zsh $ZI_HOME/github-has-repo.zsh) ]]; then
-    print "E: project already exists on github." > /dev/stderr
+    err "E: project already exists on github."
     exit $E_EXISTS
 elif [[ -d $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME ]]; then
-    print "E: local project directory exists." > /dev/stderr
+    err "E: local project directory exists."
     exit $E_EXISTS
 fi
 
-print "I: Creating project $ZI_PROJECT_NAME"
+info "I: Creating project $ZI_PROJECT_NAME"
 . $ZI_HOME/github-create-repo.zsh
 export ZI_SSH_URL=$(cat $ZI_STATE | grep SSH_URL | cut -d'=' -f2)
 . $ZI_HOME/proj-$ZI_PROJECT_TYPE.zsh
