8537642b51ea2702b8625c1ec7ac45091ea8a383
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-20 12:45:10 +0000

    Implemented delete of GitHub repo.

--- github-delete-repo.zsh
+++ github-delete-repo.zsh
@@ -1,1 +1,12 @@
-warn "this is not yet implemented"
+if [[ -z $ZI_PROJECT_NAME ]]; then
+    err "no project name"
+    exit
+fi
+
+token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
+auth="Authorization: token $token"
+
+# XXX: change hardcoded username
+url="https://api.github.com/repos/lcabrini/$ZI_PROJECT_NAME"
+response=$(curl -X DELETE -H $auth $url)
+print $response
