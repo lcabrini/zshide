8f45a31796a346f3f830b31962f6ca70280d0ae1
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-20 11:02:25 +0000

    Use functions for printing messages.

--- zi-delete-project.zsh
+++ zi-delete-project.zsh
@@ -1,9 +1,8 @@
 pd=$ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
 has_repo=$(zsh $ZI_HOME/github-has-repo.zsh)
 has_wdir=$(test -d $pd && print $pd)
-#print "REPO: $has_repo, DIR: $has_wdir"
 if [[ -z $has_repo && -z $has_wdir ]]; then
-    print "E: no such project: $ZI_PROJECT_NAME" > /dev/stderr
+    err "E: no such project: $ZI_PROJECT_NAME"
     exit 1
 fi
 print "Warning! You are about to delete $ZI_PROJECT_NAME!"
