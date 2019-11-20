990d1207bde08a8553ebc37dc42c415f2fa4018d
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-20 15:36:56 +0000

    Chaned GitHub variables.

--- github.zsh
+++ github.zsh
@@ -1,7 +1,8 @@
 token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
-ZI_GH_AUTH="-H 'Authorization: token $token'"
-ZI_GH_ACCEPT="-H 'Accept: application/vnd.github.v3+json'"
-ZI_GH_URL="https://api.github.com"
+auth="Authorization: token $token"
+accept="Accept: application/vnd.github.v3+json"
+HEADERS="-H '$auth' -H '$accept'"
+URL="https://api.github.com"
 
 # XXX: for now only used by github related functions. Move if needed.
 CURL="curl -s"
