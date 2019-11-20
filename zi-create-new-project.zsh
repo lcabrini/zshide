fa71b466d68b7dff7458d67363befab41c2d3bf2
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-20 10:13:40 +0000

    Use appropriate variables.

--- zi-create-new-project.zsh
+++ zi-create-new-project.zsh
@@ -1,7 +1,7 @@
 if [[ -z $ZI_PROJECT_TYPE ]]; then
     err "no project type specified"
     exit $E_PARAM
-elif [[ ! -f $HOME/.zshide/proj-$2.zsh ]]; then
+elif [[ ! -f $ZI_HOME/proj-$2.zsh ]]; then
     err "unknown project type: $ZI_PROJECT_TYPE"
     exit $E_PARAM
 fi
@@ -11,10 +11,10 @@ if [[ -z $ZI_PROJECT_NAME ]]; then
     exit $E_PARAM
 fi
 
-if [[ -n $(zsh $HOME/.zshide/github-has-repo.zsh) ]]; then
+if [[ -n $(zsh $ZI_HOME/github-has-repo.zsh) ]]; then
     print "E: project already exists on github." > /dev/stderr
     exit $E_EXISTS
-elif [[ -d $HOME/Git/$ZI_PROJECT_NAME ]]; then
+elif [[ -d $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME ]]; then
     print "E: local project directory exists." > /dev/stderr
     exit $E_EXISTS
 fi
@@ -22,8 +22,8 @@ fi
 print "I: Creating project $ZI_PROJECT_NAME"
 . $ZI_HOME/github-create-repo.zsh
 export ZI_SSH_URL=$(cat $ZI_STATE | grep SSH_URL | cut -d'=' -f2)
-. $HOME/.zshide/proj-$ZI_PROJECT_TYPE.zsh
-cd $HOME/Git/$ZI_PROJECT_NAME
+. $ZI_HOME/proj-$ZI_PROJECT_TYPE.zsh
+cd $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
 git add .
 git commit -m "zshide applied template" > /dev/null 2>&1
 git push > /dev/null 2>&1
