67a974f48b00ee56c2575132007c3eb6e5e3c557
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-20 15:24:13 +0000

    Clean up.

--- zi-github.zsh
+++ zi-github.zsh
@@ -1,16 +1,4 @@
-create_repo() {
-
-}
-
-delete_repo() {
-
-}
-
-has_repo() {
-
-}
-
-get_repose() {
-
-}
-
+token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
+ZI_GH_AUTH="-H 'Authorization: token $token'"
+ZI_GH_ACCEPT="-H 'Accept: application/vnd.github.v3+json'"
+ZI_GH_URL="https://api.github.com"
