a099746558096003890701ad571bb3d3ccc41543
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-21 10:55:34 +0000

    Fixed curl call for github repo delete.

--- github-delete-repo.zsh
+++ github-delete-repo.zsh
@@ -3,10 +3,12 @@ if [[ -z $ZI_PROJECT_NAME ]]; then
     exit
 fi
 
-token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
-auth="Authorization: token $token"
+#token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
+#auth="Authorization: token $token"
+
+. $ZI_HOME/github.zsh
 
 # XXX: change hardcoded username
 url="https://api.github.com/repos/lcabrini/$ZI_PROJECT_NAME"
-response=$(curl -X DELETE -H $auth $url)
+response=$(eval "$CURL -X DELETE $HEADERS $url")
 print $response
