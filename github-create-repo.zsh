7ea1c1c757b3efb2ad8749e8d993a8bb899ec362
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-21 10:42:21 +0000

    Fixed message printing.

--- github-create-repo.zsh
+++ github-create-repo.zsh
@@ -1,5 +1,5 @@
 if [[ -z $ZI_PROJECT_NAME ]]; then
-    err "E: no repository name"
+    err "no repository name"
     exit 1
 fi
 
@@ -23,17 +23,17 @@ response=$(eval "$CURL $HEADERS -d '$repodata' $url")
 ssh_url=$(print $response | jq '.ssh_url' | tr -d '"')
 info "I: created GitHub repo $ZI_PROJECT_NAME"
 (cd $ZI_PROJECTS_DIR && git clone $ssh_url > /dev/null 2>&1)
-info "I: cloned $ZI_PROJECT_NAME into $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME"
+info "cloned $ZI_PROJECT_NAME into $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME"
 
 cat >> $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME/.gitignore <<EOF
 # Editor
 .*.swp
 EOF
-print "I: applied general .gitignore"
+info "applied general .gitignore"
 
 # XXX: repository list has changed, so refresh it. Is this a good idea?
 rm $ZI_HOME/github-repos.json
 zsh $ZI_HOME/github-get-repos.zsh > /dev/null 2>&1 &
-info "I: refreshing local GitHub repo list"
+info "refreshing local GitHub repo list"
 
 print "SSH_URL=$ssh_url" >> $ZI_STATE
