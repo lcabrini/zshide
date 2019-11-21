b0f985c179072eb5ba9f66f1393b4bc038ce8c12
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-21 10:57:54 +0000

    Re-arranged code.

--- delete-project.zsh
+++ delete-project.zsh
@@ -9,11 +9,13 @@ print "Warning! You are about to delete $ZI_PROJECT_NAME!"
 print "This is an irreverible action."
 yesno "Are you sure"
 a=$?
-if [[ $a -eq 0 ]]; then
-    . $ZI_HOME/github-delete-repo.zsh
-    rm -f $ZI_HOME/github-repos.json
-    . $ZI_HOME/github-get-repos.zsh &
-    rm -rf $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
-else
-    print "do not delete"
+if [[ $a -ne 0 ]]; then
+    info "delete cancelled"
+    exit 1
 fi
+    
+. $ZI_HOME/github-delete-repo.zsh
+rm -f $ZI_HOME/github-repos.json
+. $ZI_HOME/github-get-repos.zsh &
+rm -rf $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
+
