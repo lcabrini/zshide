c993cffb77aad66888fc7af9f5d947f12dbcb64f
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-21 10:43:36 +0000

    Fixed message printing.

--- new-project.zsh
+++ new-project.zsh
@@ -7,19 +7,19 @@ elif [[ ! -f $ZI_HOME/proj-$2.zsh ]]; then
 fi
 
 if [[ -z $ZI_PROJECT_NAME ]]; then
-    err "E: no project name specified"
+    err "no project name specified"
     exit $E_PARAM
 fi
 
 if [[ -n $(zsh $ZI_HOME/github-has-repo.zsh) ]]; then
-    err "E: project already exists on github."
+    err "project already exists on github."
     exit $E_EXISTS
 elif [[ -d $ZI_PROJECTS_DIR/$ZI_PROJECT_NAME ]]; then
-    err "E: local project directory exists."
+    err "local project directory exists."
     exit $E_EXISTS
 fi
 
-info "I: Creating project $ZI_PROJECT_NAME"
+info "Creating project $ZI_PROJECT_NAME"
 . $ZI_HOME/github-create-repo.zsh
 export ZI_SSH_URL=$(cat $ZI_STATE | grep SSH_URL | cut -d'=' -f2)
 . $ZI_HOME/proj-$ZI_PROJECT_TYPE.zsh
