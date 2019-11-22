008d146a50fdf5fc3f932b4f965c87aafd4ade5b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-22 11:07:54 +0000

    Clean-up.

--- github-delete-repo.zsh
+++ github-delete-repo.zsh
@@ -3,15 +3,10 @@ if [[ -z $ZI_PROJECT_NAME ]]; then
     exit
 fi
 
-#token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
-#auth="Authorization: token $token"
-
 . $ZI_HOME/github.zsh
 
 url="https://api.github.com/repos/$GITHUB_LOGIN/$ZI_PROJECT_NAME"
 response=$(eval "$CURL -X DELETE $HEADERS $url")
 
-# XXX: repository list has changed, so refresh it. Is this a good idea?
 rm $ZI_HOME/github-repos.json
-. $ZI_HOME/github-get-repos.zsh #& #> /dev/null 2>&1 &
-#info "refreshing local GitHub repo list"
+. $ZI_HOME/github-get-repos.zsh
