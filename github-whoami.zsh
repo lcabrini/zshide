612b20199d3ede629ac9be26e2aa6ae9841c9210
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-22 09:06:54 +0000

    Don't call read_setting in a subshell.

--- github-whoami.zsh
+++ github-whoami.zsh
@@ -1,4 +1,5 @@
-if [[ -z $(read_setting GITHUB_LOGIN) ]]; then
+read_setting GITHUB_LOGIN
+if [[ -z $GITHUB_LOGIN ]]; then
     url=$URL/user
     response=$(eval "$CURL $HEADERS $url")
     username=$(print $response | jq '.login' | tr -d '"')
