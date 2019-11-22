d33bb8ed085e7765063831ac227fd35569748262
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-22 09:04:39 +0000

    Don't create a subshell to run read_setting.

--- github.zsh
+++ github.zsh
@@ -1,5 +1,12 @@
-token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
-auth="Authorization: token $token"
+. $ZI_HOME/util.zsh
+#token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
+#token=$(read_setting GITHUB_TOKEN)
+read_setting GITHUB_TOKEN
+if [[ -z $GITHUB_TOKEN ]]; then
+    err "GitHub token not found."
+    exit 1
+fi
+auth="Authorization: token $GITHUB_TOKEN"
 accept="Accept: application/vnd.github.v3+json"
 HEADERS="-H '$auth' -H '$accept'"
 URL="https://api.github.com"
@@ -7,5 +14,4 @@ URL="https://api.github.com"
 # XXX: for now only used by github related functions. Move if needed.
 CURL="curl -s"
 
-. $ZI_HOME/util.zsh
 . $ZI_HOME/github-whoami.zsh
