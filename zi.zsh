1d37a3719ebccff57c10c24139102f612647e00f
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-22 09:57:55 +0000

    Removed the export statements since they are no longer needed.

--- zi.zsh
+++ zi.zsh
@@ -23,24 +23,34 @@ if [[ $# -lt 1 ]]; then
     exit $E_COMMAND
 fi
 
-export ZI_HOME=$HOME/.zshide
-export ZI_PROJECTS_DIR=$HOME/Git
-export ZI_STATE=$(mktemp -t zshide-$USER.XXXXXX)
+ZI_HOME=$HOME/.zshide
+ZI_PROJECTS_DIR=$HOME/Git
+ZI_STATE=$(mktemp -t zshide-$USER.XXXXXX)
 
 . $ZI_HOME/util.zsh
 
 case $1 in 
     (np)
-        export ZI_PROJECT_TYPE=$2
-        export ZI_PROJECT_NAME=$3
-        export ZI_PROJECT_PATH=$ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
+        ZI_PROJECT_TYPE=$2
+        ZI_PROJECT_NAME=$3
+        ZI_PROJECT_PATH=$ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
         . $ZI_HOME/new-project.zsh
         ;;
 
     (dp)
-        export ZI_PROJECT_NAME=$2
-        export ZI_PROJECT_PATH=$ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
+        ZI_PROJECT_NAME=$2
+        ZI_PROJECT_PATH=$ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
         . $ZI_HOME/delete-project.zsh
+        ;;
+
+    (t)
+        #. $ZI_HOME/github-whoami.zsh
+        #print $(read_setting GITHUB_URL)
+        read_setting GITHUB_LOGIN
+        print "GitHub URL: $GITHUB_LOGIN"
+        #write_setting "TEST_KEY" "foobarnicous"
+        #print $(read_setting TEST_KEY)
+        exit $E_OK
         ;;
 
     (*)
