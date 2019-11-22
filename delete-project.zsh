3f5eba7649c85b7199257b5086aff2706d0fdf84
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-22 09:40:31 +0000

    Don't call github-has-repo in a subshell.

--- delete-project.zsh
+++ delete-project.zsh
@@ -1,5 +1,8 @@
 pd=$ZI_PROJECTS_DIR/$ZI_PROJECT_NAME
-has_repo=$(. $ZI_HOME/github-has-repo.zsh)
+#has_repo=$(. $ZI_HOME/github-has-repo.zsh)
+. $ZI_HOME/github-has-repo.zsh
+has_repo=$response
+
 has_wdir=$(test -d $pd && print $pd)
 if [[ -z $has_repo && -z $has_wdir ]]; then
     err "E: no such project: $ZI_PROJECT_NAME"
