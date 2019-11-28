b7c004db541cd2c5bb827dc5050884f7b4c2c6a3
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-11-28 23:49:16 +0000

    Clean up.

--- github.zsh
+++ github.zsh
@@ -6,13 +6,13 @@
 # Author: Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
 
 . $ZI_HOME/util.zsh
-#token=$(cat $ZI_HOME/tokens.txt | grep github | cut -d'=' -f2)
-#token=$(read_setting GITHUB_TOKEN)
+
 read_setting GITHUB_TOKEN
 if [[ -z $GITHUB_TOKEN ]]; then
     err "GitHub token not found."
     exit 1
 fi
+
 auth="Authorization: token $GITHUB_TOKEN"
 accept="Accept: application/vnd.github.v3+json"
 HEADERS="-H '$auth' -H '$accept'"
