1d93fd13aa2b4a05b543a540b93c0bc93cd29b82
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-01 11:26:22 +0000

    Added header comment.

--- github-has-repo.zsh
+++ github-has-repo.zsh
@@ -1,3 +1,16 @@
+# zshide: the Z Shell IDE
+#
+# Check if a repository exists on GitHub.
+# NOTE: this will probably become a function at some point.
+#
+# Author: Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
+#
+# Arguments:
+#   $1: the name of the repository
+#
+# Returns:
+#   0 if the repository was found or 1 if it wasn't
+
 if [[ -z $PROJECT_NAME ]]; then
     print "E: no repository name" >> /dev/stderr
     exit 1
